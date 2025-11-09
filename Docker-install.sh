7a9f5c8bec5e7774a489cadebdaba565c545d887
chrishac-git <64866842+chrishac-git@users.noreply.github.com>
2025-11-09 20:44:14 +0000

    Create Docker-install.sh
    purely for Chris's use if you want to you can use but at your own risk - No responsibility is taken from myself if this breaks your install / setup.

---
+++ Docker-install.sh
@@ -0,0 +1,23 @@
+# download docker install file and run it!
+# use at your own risk!
+# download this file:
+# curl -fsSl https://
+#!/bin/sh
+
+# download the script
+echo "Downloading the internet... I mean Docker Install"
+curl -fsSL https://get.docker.com -o install-docker.sh
+
+# make it executable
+echo "Give it some powers!"
+chmod +x install-docker.sh
+
+# run the install
+echo "Deleting all your hard work... Joking Running the installer!"
+sh install-docker.sh
+
+# Run hello-world
+
+Echo "saying Hi!"
+docker run hello-world
+done
